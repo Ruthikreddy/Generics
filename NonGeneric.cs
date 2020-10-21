f0a3e0cbc81572b022531edb849985049db77648
Ruthikreddy <ruthikreddy15@gmail.com>
2020-10-21 19:33:27 +0530

    [Ruthik] push for MaxThreeInteger

---
+++ NonGeneric.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindMaxProblem
+{
+    public class NonGenericClass
+    {
+        /// <summary>
+        /// Maximums the integer number among three.
+        /// </summary>
+        /// <param name="first">The first.</param>
+        /// <param name="second">The second.</param>
+        /// <param name="third">The third.</param>
+        /// <returns></returns>
+        /// <exception cref="System.Exception">All 3 numbers are same</exception>
+        public static int MaxIntegerNumberAmongThree(int first, int second, int third)
+        {
+            Console.WriteLine("Maximum number");
+            
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                return first;
+            }
+            if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                return second;
+            }
+            if (third.CompareTo(second) > 0 && third.CompareTo(first) > 0)
+            {
+                return third;
+            }
+            throw new Exception("All 3 numbers are same");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FindMaxProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to find max problem");
+            try
+            {
+                int maxInt = NonGenericClass.MaxIntegerNumberAmongThree(23, 24, 22);
+                Console.WriteLine("max integer value is\t" + maxInt);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
